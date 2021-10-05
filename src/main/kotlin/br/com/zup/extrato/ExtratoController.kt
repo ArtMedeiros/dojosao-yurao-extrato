32733ed54d6e34779a368777438e8ffbb7e5ccc6
Kleysson Silva <kleysson.silva@zup.com.br>
2021-10-05 15:51:10 -0300

    Testes do controller

--- src/main/kotlin/br/com/zup/extrato/ExtratoController.kt
+++ src/main/kotlin/br/com/zup/extrato/ExtratoController.kt
@@ -1,8 +1,6 @@
 package br.com.zup.extrato
 
-import org.jboss.logging.Logger
 import org.springframework.data.domain.PageRequest
-import org.springframework.data.domain.Pageable
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -14,16 +12,9 @@ import org.springframework.web.bind.annotation.RestController
 class ExtratoController(
     val repository: ExtratoRepository
 ) {
-
-    val log = Logger.getLogger(this::class.java)
-
     @GetMapping("/{idCliente}")
-    fun buscarExtrato(@PathVariable idCliente: Long, pageable: Pageable = PageRequest.ofSize(20)): ResponseEntity<List<ExtratoResponse>> {
-        val page = if (pageable.pageSize > 50) PageRequest.ofSize(20) else pageable
-
-        log.info("Retornando ${page.pageSize} operações realizadas pelo cliente")
-
-        return repository.findByIdClienteOrderByDataTransacaoDesc(idCliente, page)
+    fun buscarExtrato(@PathVariable idCliente: Long): ResponseEntity<List<ExtratoResponse>> {
+        return repository.findByIdClienteOrderByDataTransacaoDesc(idCliente, PageRequest.ofSize(20))
             .map { ExtratoResponse.fromExtrato(it) }
             .let { ResponseEntity.ok(it) }
     }
---
+++ src/test/kotlin/br/com/zup/extrato/ExtratoControllerTest.kt
@@ -0,0 +1,57 @@
+package br.com.zup.extrato
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import java.math.BigDecimal
+import java.time.LocalDateTime
+
+@SpringBootTest
+@AutoConfigureMockMvc
+internal class ExtratoControllerTest(
+    @Autowired val repository: ExtratoRepository,
+    @Autowired val mockMvc: MockMvc
+){
+
+    lateinit var extrato: Extrato
+
+    @BeforeEach
+    internal fun setUp() {
+        repository.deleteAll()
+
+        extrato = Extrato(
+            operacao = Operacao.PAGAMENTO_BOLETO,
+            valor = BigDecimal(125.0),
+            dataTransacao = LocalDateTime.now(),
+            idCliente = 1,
+            idConta = 1
+        )
+        repository.save(extrato)
+    }
+
+    @Test
+    internal fun `deve retornar um extrato`() {
+        mockMvc.perform(get("/api/extrato/1"))
+            .andExpect(status().isOk)
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("*operacao").value(Operacao.PAGAMENTO_BOLETO.name))
+            .andDo(print())
+    }
+
+    @Test
+    internal fun `nao deve conter nenhum dado de extrato de cliente nao cadastrado`() {
+        mockMvc.perform(get("/api/extrato/2"))
+            .andExpect(status().isOk)
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(content().string("[]"))
+            .andDo(print())
+    }
+}
