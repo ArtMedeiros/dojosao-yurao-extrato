2a0dc60c7bab1301e9918b9447a0f7aa793ee843
Kleysson Silva <kleysson.silva@zup.com.br>
2021-10-05 11:09:46 -0300

    Configuração do projeto

---
+++ src/main/kotlin/br/com/zup/extrato/ExtratoApplication.kt
@@ -0,0 +1,11 @@
+package br.com.zup.extrato
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExtratoApplication
+
+fun main(args: Array<String>) {
+	runApplication<ExtratoApplication>(*args)
+}
